dda7a8a311fc58eeea5d73e65b57b3b22c0073f1
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2022-10-12 15:08:22 -0300

    feat: adicionando ferramentas: tailwindcss, postcss, autoprefixer e storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import './styles/global.css';
+
 export function App() {
 
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      Hello World
+    </>
   )
 }
